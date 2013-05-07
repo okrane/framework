b9a28b1cc9ec0d2f91c9e070a5989ac29ddfa02d
unknown <svlasceanu@PARWS465.keplercm.lan>
2013-05-07 09:16:35 +0200

    added new params to manager

--- projects/ParameterManager/test_build_parameter_manager.py
+++ projects/ParameterManager/test_build_parameter_manager.py
@@ -7,12 +7,12 @@ collection = client.Mercure.parameter_manager
 collection.remove()
 documents = []
 
-documents.append({'strategy': "CROSS", 'key' : {}, "static_parameters": {'MinATS': 2, 'MaxATS': 5, 'dt': 100, 'blabla': 5 }})
-documents.append({'strategy': "CROSS", 'key' : {'client_id': 11}, "static_parameters": {'MinATS': 0, 'blabla': 11 }})
-documents.append({'strategy': "CROSS", 'key' : {'trader_id': 1, 'place_id': "rome"}, "static_parameters": {'MinATS': 1, 'MaxATS': 1, 'dt': 1, 'blabla': 1 }})
-documents.append({'strategy': "CROSS", 'key' : {'place_id': "paris", 'parent_algo_id': "VWAP"}, "static_parameters": {'blabla': 46 }})
-documents.append({'strategy': "VWAP", 'key' : {'place_id': "paris", 'parent_algo_id': "VWAP"}, "static_parameters": {'blabla': 1111 }})
-documents.append({'strategy': "VWAP", 'key' : {}, "static_parameters": {'blabla': 0 }})
+documents.append({'strategy': "CROSS", 'key' : {}, "static_parameters": {'MinATS': 2, 'MaxATS': 5, 'MinDarkATS': 3, "MaxDarkATS": 6, 'IndicatorRefreshTimerPeriod': 60000}})
+documents.append({'strategy': "CROSS", 'key' : {'client_id': 11}, "static_parameters": {'MinATS': 0}})
+documents.append({'strategy': "CROSS", 'key' : {'trader_id': 1, 'place_id': "rome"}, "static_parameters": {'MinATS': 1, 'MaxATS': 1}})
+documents.append({'strategy': "CROSS", 'key' : {'place_id': "paris", 'parent_algo_id': "VWAP"}, "static_parameters": {}})
+documents.append({'strategy': "VWAP", 'key' : {'place_id': "paris", 'parent_algo_id': "VWAP"}, "static_parameters": {}})
+documents.append({'strategy': "VWAP", 'key' : {}, "static_parameters": {}})
 ###########################################################
 
 for doc in documents:

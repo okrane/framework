f503be6413981f1c2e88ba210fef0e4f1b236557
Guillaume Pons <guillaume.pons@keplercm.com>
2013-05-16 11:46:44 +0200

    commit : minor commit for AlgoLauncher project

--- tests/test.py
+++ tests/test.py
@@ -1,24 +1,12 @@
-from datetime import datetime
-import paramiko
+import pyodbc
 
-if __name__ == '__main__':
-    
-    ssh = paramiko.SSHClient()
-    ssh.set_missing_host_key_policy(paramiko.AutoAddPolicy())
-    ssh.connect('172.25.152.73', username='flexsys', password='flexsys1')
-    
-    cmd = 'ls ./logs/trades'
-    (stdin, stdout_grep, stderr) = ssh.exec_command(cmd)
-    
-    for line in stdout_grep:
-        print line
-        cmd2 = "ls ./logs/trades/%s | egrep '*.fix'" %line
-        (stdin2, stdout_grep2, stderr2) = ssh.exec_command(cmd2)
-        for line2 in stdout_grep2:
-            print line2
+cnxn = pyodbc.connect('DSN=PARSRV21;UID=python_ro;PWD=python4ever!!')
+cursor = cnxn.cursor()
 
-#     date_str = '20130426-07:52:23'
-#     
-#     date = datetime.strptime(date_str, '%Y%m%d-%H:%M:%S')
-#     
-#     print date.minute
+
+cursor.execute("select top 10 * from SECURITY")
+row = cursor.fetchall()
+for r in row:
+    print r
+    
+cnxn.close()

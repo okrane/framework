df00a8e1a0f7e75a5a5e63e845ca82e9e0687f32
Silviu <svlasceanu@keplercheuvreux.com>
2013-05-31 11:48:57 +0200

    random commit

--- usr/sivla/test_functions/test_mongo_join.py
+++ usr/sivla/test_functions/test_mongo_join.py
@@ -1,12 +1,19 @@
 # -*- coding: utf-8 -*-
 
-from pymongo import MongoClient
-client = MongoClient("172.29.0.32", 27017)
-db = client.DB_test
+from lib.dbtools.connections import Connections
+db = Connections.getClient("HPP").DB_test
 
 # I want to keep only StrategyName and OrderQty
 a = dict([(y["tag_value"], y["strategy_name"]) for y in db.map_tagFIX.find({"tag_name" : "StrategyName"})])
-print a
+#print a
 b = [(a[x["StrategyName"]], x["OrderQty"]) for x in db.AlgoOrders.find({"StrategyName": {"$exists": True}, "OrderQty": {"$exists": True}})]
-print b
+#print b
     
+    
+import usr.sivla.funcs.DBTools.manycollection_query as mcq
+
+#query_sql = "select AlgoOrders.OrderQty from AlgoOrders"
+#print mcq.runQuery(query_sql, "HPP", "DB_test")
+
+query_sql = "select map_tagFIX.strategy_name, AlgoOrders.OrderQty from AlgoOrders, map_tagFIX where map_tagFIX.tag_value = AlgoOrders.StrategyName"
+print mcq.runQuery(query_sql, "HPP", "DB_test")

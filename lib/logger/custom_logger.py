ba7eb14fea61c478bb82f42bb1d860a1aafa7781
alababidi <alababidi@keplercheuvreux.com>
2013-06-26 14:55:29 +0200

    Add comments Add functionality to redirect stderr and stedout to logging Add possibility options from argv

--- lib/logger/custom_logger.py
+++ lib/logger/custom_logger.py
@@ -1,32 +1,47 @@
 import logging
-
+"""
+    Available options :
+        --loglevel=1        -> Display only all log => Critical
+        --loglevel=2        -> Display only all log => Error
+        --loglevel=3        -> Display only all log => Warning
+        --loglevel=4        -> Display only all log => Info
+        --loglevel=5        -> Display only all log => Debug
+        --to_file           -> Log also to file
+        --capture_stdout    -> redirect stdout to logging
+        --capture_stderr    -> redirect stderr to logging
+"""
 class Logger:
     __done = False
     logging = None
     def __init__(self):
         if not Logger.__done:
-            level = 1
-            to_file = False
-            
+            level           = 1
+            to_file         = False
+            capture_stdout  = False
+            capture_stderr  = True
             import sys
             for arg in sys.argv:
                 if "loglevel=" in arg.lower():
                     level =  int(arg[arg.find("loglevel=") + 9])
-                if "to_file=true" in arg.lower():
+                if "to_file" in arg.lower():
                     to_file = True
+                if "capture_stdout" in arg.lower():
+                    capture_stdout = True
+                if "capture_stderr" in arg.lower():
+                    capture_stderr = True
             
             Logger.__done = True
             
-            self.set(level, to_file)
+            self.set(level, to_file, capture_stdout, capture_stderr)
             
-    def set(self, level = 0, to_file = False):
-        if level == 2:
+    def set(self, level = 5, to_file = False, capture_stdout = False, capture_stderr = False):
+        if level == 4:
             lev = logging.INFO
         elif level == 3:
             lev = logging.WARNING
-        elif level == 4:
+        elif level == 2:
             lev = logging.ERROR
-        elif level == 5:
+        elif level == 1:
             lev = logging.CRITICAL
         else:
             lev = logging.DEBUG
@@ -58,5 +73,32 @@ class Logger:
         console.setFormatter(formatter)
         logging.getLogger('').addHandler(console)
         Logger.logging = logging
+        
+        
+        
+     
+        class StreamToLogger(object):
+            """
+            Fake file-like stream object that redirects writes to a logger instance.
+            """
+            def __init__(self, logger, log_level=logging.INFO):
+                self.logger = logger
+                self.log_level = log_level
+                self.linebuf = ''
+     
+            def write(self, buf):
+                for line in buf.rstrip().splitlines():
+                    self.logger.log(self.log_level, line.rstrip())
+        import sys     
+        stdout_logger = logging.getLogger('STDOUT')
+        sl = StreamToLogger(stdout_logger, logging.INFO)
+        if capture_stdout:
+            sys.stdout = sl
+         
+        stderr_logger = logging.getLogger('STDERR')
+        sl = StreamToLogger(stderr_logger, logging.ERROR)
+        if capture_stderr:
+            sys.stderr = sl
 
 Logger()
+

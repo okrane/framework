f9a50ec7cc9a15eb031256fb0eb6d4651b2b4be8
alababidi <alababidi@keplercheuvreux.com>
2013-08-20 16:13:42 +0100

    Improve the email

--- projects/TCA/algo_summary.py
+++ projects/TCA/algo_summary.py
@@ -1,6 +1,7 @@
 import matplotlib
 
 import os
+from email.mime.text import MIMEText
 if os.name != 'nt':
     ### VERY IMPROTANT IN LINUX (in order to use X11)
     matplotlib.use('Agg')
@@ -26,66 +27,92 @@ if __name__=='__main__':
         folder  = '/home/quant/temp/'
     l       = []
     
+    msg = MIMEMultipart('alternative')
+    msg['Subject'] = 'Algo Summary'
+    
+    
     
     # Daily
+    m = '<h2>Daily<h2>'
     daily = DataProcessor(start_date = day, end_date = day)
+    
+    
     daily.plot_algo_volume()
-    filename = folder + 'Vol_euro_from_' + datetime.strftime(day, '%Y%m%d' ) + 'to_' + datetime.strftime(day, '%Y%m%d' ) + '.png'
-    plt.savefig( filename )
-    l.append(filename)
+    image_name = 'Vol_euro_from_' + datetime.strftime(day, '%Y%m%d' ) + 'to_' + datetime.strftime(day, '%Y%m%d' ) + '.png'
+    file_path = folder + image_name
+    plt.savefig( file_path )
+    l.append(image_name)
+    
     
     daily.plot_algo_occ()
-    filename = folder + 'Occ_euro_from_' + datetime.strftime(day, '%Y%m%d' ) + 'to_' + datetime.strftime(day, '%Y%m%d' ) + '.png'
-    plt.savefig( filename )
-    l.append(filename)
+    image_name = 'Occ_euro_from_' + datetime.strftime(day, '%Y%m%d' ) + 'to_' + datetime.strftime(day, '%Y%m%d' ) + '.png'
+    file_path = folder + image_name
+    plt.savefig( file_path )
+    l.append(image_name)
+    
+    
+    
     
     # Weekly
+    m += '<h2>Weekly<h2>'
     weekly = DataProcessor(start_date = day - timedelta(days=7), end_date = day)
+    
+    
     weekly.plot_algo_volume()
-    filename = folder + 'Vol_euro_from_' + datetime.strftime(day- timedelta(days=7), '%Y%m%d' ) + 'to_' + datetime.strftime(day, '%Y%m%d' ) + '.png'
-    plt.savefig( filename )
-    l.append(filename)
+    image_name = 'Vol_euro_from_' + datetime.strftime(day- timedelta(days=7), '%Y%m%d' ) + 'to_' + datetime.strftime(day, '%Y%m%d' ) + '.png'
+    file_path = folder + image_name
+    plt.savefig( file_path )
+    l.append(image_name)
     
     weekly.plot_algo_occ()
-    filename = folder + 'Occ_euro_from_' + datetime.strftime(day- timedelta(days=7), '%Y%m%d' ) + 'to_' + datetime.strftime(day, '%Y%m%d' ) + '.png'
-    plt.savefig( filename )
-    l.append(filename)
+    image_name = 'Occ_euro_from_' + datetime.strftime(day- timedelta(days=7), '%Y%m%d' ) + 'to_' + datetime.strftime(day, '%Y%m%d' ) + '.png'
+    file_path = folder + image_name
+    plt.savefig( file_path )
+    l.append(image_name)
     
-     
+    
+    
     # Monthly
+    m += '<h2>Monthly<h2>' 
     monthly = DataProcessor(start_date = day - timedelta(days=28), end_date = day)
+    
+    
     monthly.plot_algo_volume()
-    filename = folder + 'Vol_euro_from_' + datetime.strftime(day - timedelta(days=28), '%Y%m%d' ) + 'to_' + datetime.strftime(day, '%Y%m%d' ) + '.png'
-    plt.savefig( filename )
-    l.append(filename)
+    image_name = 'Vol_euro_from_' + datetime.strftime(day - timedelta(days=28), '%Y%m%d' ) + 'to_' + datetime.strftime(day, '%Y%m%d' ) + '.png'
+    file_path = folder + image_name
+    plt.savefig( file_path )
+    l.append(image_name)
     
     
     monthly.plot_algo_occ()
-    filename = folder + 'Occ_euro_from_' + datetime.strftime(day - timedelta(days=28), '%Y%m%d' ) + 'to_' + datetime.strftime(day, '%Y%m%d' ) + '.png'
-    plt.savefig( filename )
-    l.append(filename)
+    image_name = 'Occ_euro_from_' + datetime.strftime(day - timedelta(days=28), '%Y%m%d' ) + 'to_' + datetime.strftime(day, '%Y%m%d' ) + '.png'
+    file_path = folder + image_name
+    plt.savefig( file_path )
+    l.append(image_name)
     
     # Send an email
     
-    title = "<h2>Sum up for Yesterday %s</h2>\n" % datetime.strftime(day, '%Y%m%d' )
+    title = "<h2>Sum up for %s</h2>\n" % datetime.strftime(day, '%Y%m%d' )
         
     # Create the container (outer) email message.
-    msg = MIMEMultipart()
-    msg['Subject'] = 'test'
+    
+    
     # me == the sender's email address
     # family = the list of all recipients' email addresses
     msg['From'] = 'alababidi@keplercheuvreux.com'
-    msg['To'] = 'alababidi@keplercheuvreux.com'
-    
+    to = ['njoseph@keplercheuvreux.com', 'alababidi@keplercheuvreux.com']
+    msg['To'] = ' ,'.join(to)
     # Assume we know that the image files are all in PNG format
     for file in l:
         # Open the files in binary mode.  Let the MIMEImage class automatically
         # guess the specific image type.
-        fp = open(file, 'rb')
+        m += '<b>Some <i>HTML</i> text</b> and an image.<br><img src="cid:%s"><br>Nifty!'%file
+        fp = open(folder + file, 'rb')
         img = MIMEImage(fp.read())
+        img.add_header('Content-ID', '<%s>'%file)
         fp.close()
         msg.attach(img)
     # Send the email via our own SMTP server.
     s = smtplib.SMTP('172.29.97.16')
-    s.sendmail(msg['To'], msg['From'], msg.as_string())
+    s.sendmail(to, msg['From'], msg.as_string())
     s.quit()
